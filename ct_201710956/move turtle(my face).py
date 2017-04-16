0f8070fb9c6a51cf1286106becf42f9a190411d9
YJ17 <pl3434on@gmail.com>
2017-04-17 01:16:49 +0900

    Add files via upload

---
+++ ct_201710956/move turtle(my face).py
@@ -0,0 +1,15 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 17:54:52) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> wn = turtle.Screen()
+>>> t1 = turtle.Turtle()
+>>> t1.fd(100)
+>>> wn.addshape("C:\\Users\\USER\\Documents\\Leeyoujeong.gif")
+>>> t1.shape("C:\\Users\\USER\\Documents\\Leeyoujeong.gif")
+>>> t1.rt(90)
+>>> t1.fd(100)
+>>> t1.rt(90)
+>>> t1.fd(100)
+>>> t1.rt(90)
+>>> t1.fd(100)
+>>> 
